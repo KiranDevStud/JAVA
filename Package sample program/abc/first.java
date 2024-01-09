21cfe23b737e5f484cff409a36bd685cf68da79a
Kiran S <kiransofficial16@gmail.com>
2024-01-09 21:14:01 +0530

    user defined package program

---
+++ Package sample program/abc/first.java
@@ -0,0 +1,9 @@
+package abc;
+public class first
+{
+int a=1;
+int b=4;
+public void add(){
+System.out.println(a+b);
+}
+}
---
+++ Package sample program/abc/second.java
@@ -0,0 +1,9 @@
+package abc;
+public class second{
+int a=10;
+int b=4;
+public void div()
+{
+System.out.println(a-b);
+}
+}
