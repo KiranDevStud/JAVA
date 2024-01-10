c5e7fdbce172c2bd7ba653989c16d078ee71b2f6
Kiran S <kiransofficial16@gmail.com>
2024-01-10 16:43:58 +0530

    Delete Package sample program directory

--- Package sample program/abc/first.java
+++
@@ -1,9 +0,0 @@
-package abc;
-public class first
-{
-int a=1;
-int b=4;
-public void add(){
-System.out.println(a+b);
-}
-}
--- Package sample program/abc/second.java
+++
@@ -1,9 +0,0 @@
-package abc;
-public class second{
-int a=10;
-int b=4;
-public void div()
-{
-System.out.println(a-b);
-}
-}
